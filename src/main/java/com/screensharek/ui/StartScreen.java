4090b05aa009bc334d0433eb3f2658138f37f8d1
E-iLiCiTaNo <reymisterio_jose@hotmail.es>
2021-03-14 02:24:58 +0100

    Starting the design of the main screen.

--- src/main/java/com/screensharek/ui/StartScreen.java
+++ src/main/java/com/screensharek/ui/StartScreen.java
@@ -4,41 +4,96 @@ import javax.swing.*;
 import java.awt.*;
 
 public class StartScreen {
+
     private JFrame frame;
+    private JPanel panelButton;
     private JButton connect;
     private JButton shareScreen;
+    private Font font;
+    private BoxLayout frameLayout;
 
+    /**
+     * Initialize the class and configure the components.
+     */
     public void init() {
         startComponents();
         configureFrame();
+        configurePanelButton();
+        configureShareButton();
         configureConnectButton();
         frame.setVisible(true);
     }
 
+    /**
+     * Create components.
+     */
     public void startComponents() {
         frame = new JFrame();
         connect = new JButton();
         shareScreen = new JButton();
+        font = new Font("a Aha Wow", Font.PLAIN, 24);
+        panelButton = new JPanel();
+        frameLayout = new BoxLayout(panelButton, BoxLayout.Y_AXIS);
     }
 
+    /**
+     * Configure the frame.
+     */
     public void configureFrame() {
         frame.setSize(new Dimension(935, 436));
-        frame.getContentPane().setBackground(new Color(30, 30, 30));
         frame.setResizable(false);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setLayout(null);
     }
 
+    /**
+     * Configure the main panel.
+     */
+    public void configurePanelButton() {
+        panelButton.setLayout(frameLayout);
+        panelButton.setBackground(new Color(30, 30, 30));
+        frame.add(panelButton);
+    }
+
+    /**
+     * Configure the button to connect a remote host.
+     */
     public void configureConnectButton() {
-        Icon iconHover = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\TestButtonHover.png");
-        Icon icon = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\TestButton.png");
+        Icon iconHover = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\ButtonMainScreenHover.png");
+        Icon icon = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\ButtonMainScreen.png");
+
         connect.setIcon(icon);
         connect.setRolloverIcon(iconHover);
-        connect.setBounds(250, 250, 414, 88);
+        connect.setForeground(new Color(0, 0, 0));
+        connect.setPreferredSize(new Dimension(414, 88));
+        connect.setFont(font);
         connect.setBorderPainted(false);
         connect.setContentAreaFilled(false);
-        //connect.setPreferredSize(new Dimension(414, 88));
-        frame.add(connect);
+        connect.setText("CONECTARSE A UN ANFITRION");
+        connect.setHorizontalTextPosition(SwingConstants.CENTER);
+        connect.setAlignmentX(Component.CENTER_ALIGNMENT);
+        connect.setFocusable(false);
+        panelButton.add(connect);
+        panelButton.add(Box.createRigidArea(new Dimension(0,65)));
+    }
+
+    /**
+     * Configure the button to start to share the screen.
+     */
+    public void configureShareButton() {
+        Icon iconHover = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\ButtonMainScreenHover.png");
+        Icon icon = new ImageIcon("C:\\Users\\Jose\\Desktop\\Proyecto21\\ScreenSharek\\src\\main\\resources\\ButtonMainScreen.png");
+        shareScreen.setIcon(icon);
+        shareScreen.setRolloverIcon(iconHover);
+        shareScreen.setForeground(new Color(0, 0, 0));
+        shareScreen.setPreferredSize(new Dimension(414, 88));
+        shareScreen.setFont(font);
+        shareScreen.setBorderPainted(false);
+        shareScreen.setContentAreaFilled(false);
+        shareScreen.setText("COMPARTIR PANTALLA");
+        shareScreen.setHorizontalTextPosition(SwingConstants.CENTER);
+        shareScreen.setAlignmentX(Component.CENTER_ALIGNMENT);
+        shareScreen.setFocusable(false);
+        panelButton.add(shareScreen);
     }
 }
